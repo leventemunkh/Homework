76ab025ace08519e9ebc50c55ca70e5864e407de
leventemunkh <leventem96@gmail.com>
2023-09-17 16:00:31 +0200

    kezdes

---
+++ demo-munka/src/components/MainComponent.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Header from './components/HeaderComponent'
+import Footer from './components/FooterComponent'
+
+function Main() {
+    return (
+
+        <>
+        <Header/>
+        <div>Hello world</div>
+        <Footer/>
+        </>
+
+    )
+    
+}
+
+export default Main;
