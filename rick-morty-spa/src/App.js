bf52a049d7ac90383afce13660aeac55407f0280
leventemunkh <leventem96@gmail.com>
2023-09-19 13:40:05 +0200

    kezdes

---
+++ rick-morty-spa/src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-start raleway my-3 mx-3">Rick and Morty SPA</h1>
+    </div>
+  );
+}
+
+export default App;
