058e4eed0a3c32da9b35955eb81ad32b0dc0af46
leventemunkh <leventem96@gmail.com>
2023-09-19 14:05:07 +0200

    .

--- rick-morty-spa/src/App.js
+++ rick-morty-spa/src/App.js
@@ -1,10 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import Cards from "./components/Cards/Cards";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-start raleway my-3 mx-3">Rick and Morty SPA</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-10">
+            <div className="row">
+              <Cards />
+              <Cards />
+              <Cards />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
